9e4e46e5689c79a5b7cf47ae61dd5dfb7709ca46
Anna Valerio <anna.j.neff@gmail.com>
2024-08-15 20:37:26 -0500

    visibility still not passing tests; working on pickPlanet functionality

--- script.js
+++ script.js
@@ -23,19 +23,18 @@ window.addEventListener("load", function() {
     });
     
     
-    
-
-    // let listedPlanets;
+    let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    // let listedPlanetsResponse = myFetch();
-    // listedPlanetsResponse.then(function (result) {
-    //     listedPlanets = result;
-        // console.log(listedPlanets); - logs undefined
-    // }).then(function () {
-        // console.log(listedPlanets);
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    // })
+    // console.log(listedPlanets[0]); --returns undefined. Why can't it select an index?
     let planet = pickPlanet(listedPlanets);
-//     return planet;
-//  });
+    console.log(planet);
+    addDestinationInfo(document, name, diameter, star, distance, moons, imageURL);
+    })
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -2,22 +2,24 @@
 
 require('cross-fetch/polyfill');
 
-// function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-    // Here is the HTML formatting for our mission target div.
-    /*
-                 <h2>Mission Destination</h2>
-                 <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
-                     <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
-                 </ol>
-                 <img src="">
-    */
-//  }
+function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+    let missionTarget = document.getElementById("missionTarget");
+        missionTarget.innerHTML = 
 
-// let missionTarget = document.getElementById("missionTarget");
+    // Here is the HTML formatting for our mission target div.
+    
+                 `<h2>Mission Destination</h2>
+                 <ol>
+                     <li>Name: ${name}</li>
+                     <li>Diameter: ${diameter}</li>
+                     <li>Star: ${star}</li>
+                     <li>Distance from Earth: ${distance}</li>
+                     <li>Number of Moons: ${moons}</li>
+                 </ol>
+                 <img src="${imageURL}">`
+    
+ }
+
  
  function validateInput(testInput) {
     if (testInput === ""){
@@ -129,23 +131,26 @@ require('cross-fetch/polyfill');
  }
  
  async function myFetch() {
-//      let planetsReturned;
+     let planetsReturned;
  
-//      planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
-//         // response.json().then(function(json){
-//             console.log(json);
-//         // });     
-//     });
- 
-//      return planetsReturned;
- }
+     planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {   
+     return response;
+            
+        });   
+        console.log(planetsReturned); 
+        return planetsReturned; 
+    }
  
  function pickPlanet(planets) {
-//     let planet = planets[Math.floor(Math.random() * (planets.length))];
-//     return planet;
+    // planet = planets[Math.floor(Math.random() * 6)];
+    // why does this not allow me to log an index from the array? The whole array logs correctly 
+    // but I can't seem to use array methods on it
+    planet = planets[0];
+    console.log(planet);
+    return planet;
  }
  
-//  module.exports.addDestinationInfo = addDestinationInfo;
+ module.exports.addDestinationInfo = addDestinationInfo;
  module.exports.validateInput = validateInput;
  module.exports.formSubmission = formSubmission;
  module.exports.pickPlanet = pickPlanet;
